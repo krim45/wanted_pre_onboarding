0c3f46d76043df9afb3dbbf11fe4203c4fd5c8fd
krim45 <krim45@naver.com>
2022-01-31 20:58:15 +0900

    :art:style: App 기본 틀 구성 - App.css 조정 - 기본 HTML 레이아웃 설정

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,9 +1,43 @@
+import Toggle from './component/Toggle';
+import Modal from './component/Modal';
+import Tab from './component/Tab';
+import Tag from './component/Tag';
+import AutoComplete from './component/AutoComplete';
+import ClickToEdit from './component/ClickToEdit';
 import './App.css';
 
 const App = () => {
   return (
-    <div>
-      Hello world
+    <div className="container">
+      <section className="box">
+        <div className="box-title">Toggle</div>
+        <Toggle />
+      </section>
+
+      <section className="box">
+        <div className="box-title">Modal</div>
+        <Modal />
+      </section>
+
+      <section className="box">
+        <div className="box-title">Tab</div>
+        <Tab />
+      </section>
+
+      <section className="box">
+        <div className="box-title">Tag</div>
+        <Tag />
+      </section>
+
+      <section className="box">
+        <div className="box-title">AutoComplete</div>
+        <AutoComplete />
+      </section>
+
+      <section className="box">
+        <div className="box-title">ClickToEdit</div>
+        <ClickToEdit />
+      </section>
     </div>
   );
 }
