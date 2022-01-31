bb4fdcec24b85a8d10c822dffaddfdc2d4865480
krim45 <krim45@naver.com>
2022-01-31 21:46:13 +0900

    :sparkles:feat: Toggle UI 구현 - Toggle 상태 추가 - Toggle 상태 확인을 위한 text 추가 - 기본 레이아웃 설정 - 가상요소 before를 사용한 토글 백그라운드 색 변경

--- custom-component/src/component/Toggle.js
+++ custom-component/src/component/Toggle.js
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .toggle-container {
+    width: 80px;
+    height: 35px;
+    background-color: lightgray;
+    border-radius: 30px;
+    position: relative;
+    overflow: hidden;
+
+    ::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: purple;
+      transition: 0.3s linear;
+      transform: ${props => props.isToggle ? 'translateX(0%)' : 'translateX(-100%)'};
+    }
+  }
+
+  .toggle-circle {
+    width: 25px;
+    height: 25px;
+    background-color: white;
+    border-radius: 50%;
+    position: absolute;
+    top: 5px;
+    left: ${props => props.isToggle ? '50px' : '5px'};
+    transition: 0.3s linear;
+  }
+
+  .text {
+    margin: 10px 0;
+  }
+`;
 
 const Toggle = () => {
-  return (
-    <div>
+  const [isToggle, setIsToggle] = useState(false);
 
-    </div>
+  const toggleHandler = () => {
+    setIsToggle(!isToggle);
+  }
+
+  return (
+    <Wrapper isToggle={isToggle}>
+      <div className='toggle-container' onClick={toggleHandler}>
+        <div className='toggle-circle'></div>
+      </div>
+
+      <div className='text'>Toggle Switch {isToggle ? 'ON' : 'OFF'}</div>
+    </Wrapper>
   );
 };
 
